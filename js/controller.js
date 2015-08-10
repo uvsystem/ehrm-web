76510592c4f0fe7dc4d8d063f4e6b8561a69a547
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-08-10 08:47:50 +0800

    Rename file.

--- js/controller.js
+++ js/controller.js
@@ -107,7 +107,7 @@ $( document ).ready( function () {
 	$( document ).on( 'click', '#nav-logout', function() {
 		
 		page.change( $( '#message' ), '');
-		restAdapter.logout();
+		ehrmRestAdapter.logout();
 
 	} );
 	
